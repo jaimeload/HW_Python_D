bb5611e2bdc034d9996a2b340b27b921ecd21fa0
jaimeload <jaime.si.load@gmail.com>
2024-02-09 12:16:05 +0500

    commit1

---
+++ hw2/2.py
@@ -0,0 +1,14 @@
+num = int(input('Введите число: '))
+result = ''
+HEX = 16
+hex_dict = {10: 'a', 11: 'b', 12: 'c', 13: 'd', 14: 'e', 15: 'f'}
+
+print(hex(num))
+
+while num > 0:
+    res = num % HEX
+    if res in hex_dict:
+        res = hex_dict.get(res)
+    result = str(res) + result
+    num = num // HEX
+print('0x' + result)
